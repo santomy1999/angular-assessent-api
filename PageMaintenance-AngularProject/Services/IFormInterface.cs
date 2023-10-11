6a466036345fc824c08b8601d3b11a61c2c938d4
Santo M Y <santomy1999@gmail.com>
2023-10-11 11:59:47 +0530

    more null check

--- PageMaintenance-AngularProject/Services/IFormInterface.cs
+++ PageMaintenance-AngularProject/Services/IFormInterface.cs
@@ -4,16 +4,17 @@ namespace PageMaintenance_AngularProject.Services
 {
     public interface IFormInterface
     {
-        Task<Form> AddForm(Form form);
-        Task<Form> DeleteFormById(Guid formId);
-        Task<Form> EditFormById(Form newForm);
-        //Task<List<Form>?> GetAllForms();
+        Task<Form?> AddForm(Form form);
+
+        Task<Form?> DeleteFormById(Guid formId);
+        Task<Form?> EditFormById(Form newForm);
+
+        
         Task<List<Form>?> GetAllForms(int pageNumber, int pageSize);
         Task<List<Form>?> GetFormByFormName(string formName, int pageNumber, int pageSize);
         Task<List<Form>?> GetFormByFormNumber(string formNumber, int pageNumber, int pageSize);
 
-        //Task<List<Form>?> GetFormByFormName(string formName);
-        //Task<List<Form>?> GetFormByFormNumber(string formNumber);
-        Task<Form> GetFormById(Guid id);
+        
+        Task<Form?> GetFormById(Guid id);
     }
 }
